98be961c5286b373ade3f90230efa2775f6a236e
havvasumeyyekilic <102115110+havvasumeyyekilic@users.noreply.github.com>
2022-05-04 15:45:18 +0200

    L06Fin

---
+++ Aufgaben/L06/script.js
@@ -0,0 +1,12 @@
+// "Die Einwohnerzahl in Europa beträgt im Jahr 2022: XXX Mio." "Relativ zur Gesamtzahl der Einwohnerzahl in der EU ist die Einwohnerzahl in Deutschland im Jahr 2022: XXX %" "Für Deutschland hat sich die Einwohnerzahl im Jahr 2022 im Vergleich zu 2008 um XXX % verändert"
+const deutschland2008 = 82;
+const deutschland2022 = 83.2;
+const frankreich2008 = 62.14;
+const frankreich2022 = 65.48;
+const italien2008 = 59;
+const italien2022 = 59.53;
+const niderlande2008 = 16.41;
+const niederlande2022 = 17.51;
+const europa2022 = 447.01;
+console.log("Gesamtzahl EinwohnerInnen in Deutschland in 2022" + deutschland2022);
+//# sourceMappingURL=script.js.map
