fa02b83d203dbfdddb0da8d31d739222319b31a6
havvasumeyyekilic <102115110+havvasumeyyekilic@users.noreply.github.com>
2022-05-06 15:55:04 +0200

    CORRECT:Konsole

--- Aufgaben/L06/script.ts
+++ Aufgaben/L06/script.ts
@@ -6,26 +6,9 @@ const frankreich2008: number = 62.14;
 const frankreich2022: number = 65.48;
 const italien2008: number = 59;
 const italien2022: number = 59.53;
-const niderlande2008: number = 16.41;
+const niederlande2008: number = 16.41;
 const niederlande2022: number = 17.51;
 const europa2022: number = 447.01;
-
-console.log("Gesamtzahl EinwohnerInnen in Deutschland in 2022" + ": "  + deutschland2022 + "Mio.");
-console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + deutschland2022Anteileuropa2022 + "%");
-console.log("Wachstumsrate in Deutschland seit 2008" + ":" + " " + deutschlandWachstum2008 + "%");
-
-console.log("Gesamtzahl EinwohnerInnen in Frankreich in 2022" + ": "  + frankreich2022 + "Mio.");
-console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + frankreich2022Anteileuropa2022 + "%");
-console.log("Wachstumsrate in Frankreich seit 2008" + ":" + " " + frankreichWachstum2008 + "%");
-
-console.log("Gesamtzahl EinwohnerInnen in Italien in 2022" + ": "  + italien2022 + "Mio.");
-console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + italien2022Anteileuropa2022 + "%");
-console.log("Wachstumsrate in Italien seit 2008" + ":" + " " + italienWachstum2008 + "%");
-
-console.log("Gesamtzahl EinwohnerInnen in Niederlande in 2022" + ": "  + niederlande2022 + "Mio.");
-console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + niederlande2022Anteileuropa2022 + "%");
-console.log("Wachstumsrate in Niederlande seit 2008" + ":" + " " + niederlandeWachstum2008 + "%");
-
 
 const deutschland2022Anteileuropa2022: number = deutschland2022 / europa2022 * 100;
 const frankreich2022Anteileuropa2022: number = frankreich2022 / europa2022 * 100;
@@ -44,3 +27,19 @@ const italienWachstum2008: number = italienDifferenz / italien2022 * 100;
 
 const niederlandeDifferenz: number = niederlande2022 - niederlande2008;
 const niederlandeWachstum2008: number = niederlandeDifferenz / niederlande2022 * 100;
+
+console.log("Gesamtzahl EinwohnerInnen in Deutschland in 2022" + ": "  + deutschland2022 + "Mio.");
+console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + deutschland2022Anteileuropa2022 + "%");
+console.log("Wachstumsrate in Deutschland seit 2008" + ":" + " " + deutschlandWachstum2008 + "%");
+
+console.log("Gesamtzahl EinwohnerInnen in Frankreich in 2022" + ": "  + frankreich2022 + "Mio.");
+console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + frankreich2022Anteileuropa2022 + "%");
+console.log("Wachstumsrate in Frankreich seit 2008" + ":" + " " + frankreichWachstum2008 + "%");
+
+console.log("Gesamtzahl EinwohnerInnen in Italien in 2022" + ": "  + italien2022 + "Mio.");
+console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + italien2022Anteileuropa2022 + "%");
+console.log("Wachstumsrate in Italien seit 2008" + ":" + " " + italienWachstum2008 + "%");
+
+console.log("Gesamtzahl EinwohnerInnen in Niederlande in 2022" + ": "  + niederlande2022 + "Mio.");
+console.log("Relativ zu Gesamtzahl in der EU 2022" + ":" + " " + niederlande2022Anteileuropa2022 + "%");
+console.log("Wachstumsrate in Niederlande seit 2008" + ":" + " " + niederlandeWachstum2008 + "%");
