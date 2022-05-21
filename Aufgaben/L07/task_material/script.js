f49441e79768b1aff4f5d1710c9e9bee5c1b77b7
havvasumeyyekilic <102115110+havvasumeyyekilic@users.noreply.github.com>
2022-05-21 20:44:49 +0200

    L08:  FINISH

--- Aufgaben/L07/task_material/script.js
+++ Aufgaben/L07/task_material/script.js
@@ -1,62 +1,96 @@
-const land01 = "Deutschland";
-const deutschland08 = 82;
-const deutschland22 = 83.2;
-const land02 = "Frankreich";
-const frankreich08 = 62.14;
-const frankreich22 = 65.48;
-const land03 = "Italien";
-const italien08 = 59;
-const italien22 = 65.48;
-const land04 = "Niederlande";
-const niederlande08 = 16.41;
-const niederlande22 = 17.51;
-const europa22 = 447.01;
-const jahr = 2022;
-function gesamtzahl(land, land22) {
-    console.log("Gesamtzahl EinwohnerInnen in " + land + " " + jahr + ": " + land22 + "Mio.");
-}
-;
-gesamtzahl(land01, deutschland22);
-gesamtzahl(land02, frankreich22);
-gesamtzahl(land03, italien22);
-gesamtzahl(land04, niederlande22);
-function anteilEuropa22(jahr, land22, land) {
-    const berechnungnr1 = land22 / europa22 * 100;
-    console.log("Relativ zur Gesamtzahl in der EU " + jahr + ": " + berechnungnr1 + "%" + " in" + land);
-}
-;
-anteilEuropa22(jahr, deutschland22, land01);
-anteilEuropa22(jahr, frankreich22, land02);
-anteilEuropa22(jahr, italien22, land03);
-anteilEuropa22(jahr, niederlande22, land04);
-function wachstum(land22, land08, land) {
-    const berechnungnr2 = land22 - land08;
-    const berechnungnr3 = berechnungnr2 / land22 * 100;
-    console.log("Wachstumsrate in " + land + " seit 2008: " + berechnungnr3 + "%");
-}
-;
-wachstum(deutschland22, deutschland08, land01);
-wachstum(frankreich22, frankreich08, land02);
-wachstum(italien22, italien08, land03);
-wachstum(niederlande22, niederlande08, land04);
-document.querySelector("#deutschland").addEventListener("click", bildClicked1);
-function bildClicked1() {
-    document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Deutschland";
-}
-;
-document.querySelector("#frankreich").addEventListener("click", bildClicked2);
-function bildClicked2() {
-    document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Frankreich";
-}
-;
-document.querySelector("#italien").addEventListener("click", bildClicked3);
-function bildClicked3() {
-    document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Italien";
-}
-;
-document.querySelector("#niederlande").addEventListener("click", bildClicked4);
-function bildClicked4() {
-    document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Niederlande";
-}
-;
+var L07;
+(function (L07) {
+    const deutschland2008 = 82;
+    const deutschland2022 = 83.2;
+    const frankreich2008 = 62.14;
+    const frankreich2022 = 65.48;
+    const italien2008 = 59;
+    const italien2022 = 65.48;
+    const niederlande2008 = 16.41;
+    const niederlande2022 = 17.51;
+    const europa2022 = 447.01;
+    const europa2008 = 440.66;
+    const land01 = "Deutschland";
+    const land02 = "Frankreich";
+    const land03 = "Italien";
+    const land04 = "Niederlande";
+    const anteileuInEU = europa2022 / europa2022 * 100;
+    const anteildeInEU = deutschland2022 / europa2022 * 100;
+    const anteilfrInEU = frankreich2022 / europa2022 * 100;
+    const anteilitInEU = italien2022 / europa2022 * 100;
+    const anteilnlInEU = niederlande2022 / europa2022 * 100;
+    const wachstumeuSeit2008inProzent = (europa2022 - europa2008) / europa2008;
+    const wachstumdeSeit2008inProzent = (deutschland2022 - deutschland2008) / deutschland2008;
+    const wachstumfrSeit2008inProzent = (frankreich2022 - frankreich2008) / frankreich2008;
+    const wachstumitSeit2008inProzent = (italien2022 - italien2008) / italien2008;
+    const wachstumnlSeit2008inProzent = (niederlande2022 - niederlande2008) / niederlande2008;
+    const differenzeu08und22 = europa2022 - europa2008;
+    const differenzde08und22 = deutschland2022 - deutschland2008;
+    const differenzfr08und22 = frankreich2022 - frankreich2008;
+    const differenzit08und22 = italien2022 - italien2008;
+    const differenznl08und22 = niederlande2022 - niederlande2008;
+    function Deutschland() {
+        document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Deutschland";
+        document.querySelector(".chart").setAttribute("style", "height" + anteildeInEU);
+        document.querySelector(".germany").setAttribute("style", "opacity:1");
+        document.getElementById("#gesamtzahleinwohner").innerHTML = europa2022 + "Mio.";
+        document.getElementById("#info").innerHTML = "Gesamtzahl EinwohnerInnen in Deutschland in 2022";
+        document.getElementById("#relativgesamtzahl").innerHTML = anteildeInEU.toFixed(2) + "%";
+        document.getElementById("#wachstumsrate").innerHTML = wachstumdeSeit2008inProzent.toFixed(2) + "%";
+        document.getElementById("#differenz").innerHTML = differenzde08und22.toFixed(2) + "Mio.";
+        document.querySelector(".chart").setAttribute("style", "height:" + anteildeInEU + "%");
+    }
+    function Frankreich() {
+        document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Frankreich";
+        document.querySelector(".chart").setAttribute("style", "height" + anteilfrInEU);
+        document.querySelector(".france").setAttribute("style", "opacity:1");
+        document.getElementById("#gesamtzahleinwohner").innerHTML = europa2022 + "Mio.";
+        document.getElementById("#info").innerHTML = "Gesamtzahl EinwohnerInnen in Frankreich in 2022";
+        document.getElementById("#relativgesamtzahl").innerHTML = anteilfrInEU.toFixed(2) + "%";
+        document.getElementById("#wachstumsrate").innerHTML = wachstumfrSeit2008inProzent.toFixed(2) + "%";
+        document.getElementById("#differenz").innerHTML = differenzfr08und22.toFixed(2) + "Mio.";
+        document.querySelector(".chart").setAttribute("style", "height:" + anteilfrInEU + "%");
+    }
+    function Italien() {
+        document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Italien";
+        document.querySelector(".chart").setAttribute("style", "height" + anteilitInEU);
+        document.querySelector(".italy").setAttribute("style", "opacity:1");
+        document.getElementById("#gesamtzahleinwohner").innerHTML = europa2022 + "Mio.";
+        document.getElementById("#info").innerHTML = "Gesamtzahl EinwohnerInnen in Italien in 2022";
+        document.getElementById("#relativgesamtzahl").innerHTML = anteilitInEU.toFixed(2) + "%";
+        document.getElementById("#wachstumsrate").innerHTML = wachstumitSeit2008inProzent.toFixed(2) + "%";
+        document.getElementById("#differenz").innerHTML = differenzit08und22.toFixed(2) + "Mio.";
+        document.querySelector(".chart").setAttribute("style", "height:" + anteilitInEU + "%");
+    }
+    function Niederlande() {
+        document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Niederlande";
+        document.querySelector(".chart").setAttribute("style", "height" + anteilnlInEU);
+        document.querySelector(".netherlands").setAttribute("style", "opacity:1");
+        document.getElementById("#gesamtzahleinwohner").innerHTML = europa2022 + "Mio.";
+        document.getElementById("#info").innerHTML = "Gesamtzahl EinwohnerInnen in Niederlande in 2022";
+        document.getElementById("#relativgesamtzahl").innerHTML = anteilnlInEU.toFixed(2) + "%";
+        document.getElementById("#wachstumsrate").innerHTML = wachstumnlSeit2008inProzent.toFixed(2) + "%";
+        document.getElementById("#differenz").innerHTML = differenznl08und22.toFixed(2) + "Mio.";
+        document.querySelector(".chart").setAttribute("style", "height:" + anteilnlInEU + "%");
+    }
+    function Europa() {
+        document.querySelector(".einwohnerzahl").innerHTML = "Einwohnerzahl in Europa";
+        document.querySelector(".chart").setAttribute("style", "height" + anteileuInEU);
+        document.querySelector(".europe").setAttribute("style", "opacity:1");
+        document.getElementById("#gesamtzahleinwohner").innerHTML = europa2022 + "Mio.";
+        document.getElementById("#info").innerHTML = "Gesamtzahl EinwohnerInnen in Europa in 2022";
+        document.getElementById("#relativgesamtzahl").innerHTML = anteileuInEU.toFixed(2) + "%";
+        document.getElementById("#wachstumsrate").innerHTML = wachstumeuSeit2008inProzent.toFixed(2) + "%";
+        document.getElementById("#differenz").innerHTML = differenzeu08und22.toFixed(2) + "Mio.";
+        document.querySelector(".chart").setAttribute("style", "height:" + anteileuInEU + "%");
+    }
+    window.addEventListener("load", function () {
+        document.querySelector("#chartStarWrapper").addEventListener("click", Europa);
+        document.querySelector(".deutschland").addEventListener("click", Deutschland);
+        document.querySelector(".frankreich").addEventListener("click", Frankreich);
+        document.querySelector(".italien").addEventListener("click", Italien);
+        document.querySelector(".niederlande").addEventListener("click", Niederlande);
+        document.querySelector(".stars").setAttribute("style", "opacity: " + "0.5");
+    });
+})(L07 || (L07 = {}));
 //# sourceMappingURL=script.js.map
