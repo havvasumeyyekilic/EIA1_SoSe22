09bdf807c7aff10f328a60bcfc525c6477515a59
havvasumeyyekilic <102115110+havvasumeyyekilic@users.noreply.github.com>
2022-09-28 22:41:40 +0200

    delete

---
+++ IGotThis/notiz.js
@@ -0,0 +1,14 @@
+const got7 = 7;
+const jb = 1;
+var var1 = "C";
+switch (var1) {
+    case "A":
+        console.log("var1 is A");
+        break; // nicht vergesen sonst dauerschleife
+    case "B":
+        console.log("var1 is B");
+        break;
+    default:
+        console.log("var1 is sth else");
+}
+//# sourceMappingURL=notiz.js.map
---
+++ IGotThis/notiz.ts
@@ -0,0 +1,17 @@
+const got7: number = 7;
+const jb: number = 1;
+
+var var1: string = "C";
+
+switch (var1) {
+    case "A":
+        console.log("var1 is A");
+        break; // nicht vergesen sonst dauerschleife
+    case "B":
+        console.log("var1 is B");
+        break;
+    default:
+        console.log("var1 is sth else");
+}
+
+
